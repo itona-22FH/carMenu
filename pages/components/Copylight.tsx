24e6f473d2ca49b4688bae4555640cfaa4c3323a
itona <naov7700@gmail.com>
2023-06-06 23:28:50 +0900

    add Copylight

---
+++ pages/components/Copylight.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box, Flex } from "@chakra-ui/react";
+
+export const Copylight = () => {
+  return (
+    <Flex h="40px" mt="20px" bg="#BDE9FD" justifyContent="center" alignItems="center">
+      Copylight
+    </Flex>
+  );
+};
