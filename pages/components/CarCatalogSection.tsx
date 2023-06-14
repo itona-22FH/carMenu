bd3bf08734bcb3648d7a91df408f2ad3b44de50b
itona <naov7700@gmail.com>
2023-06-14 22:45:51 +0900

    スライダー　スタイル　修正

--- pages/components/CarCatalogSection.tsx
+++ pages/components/CarCatalogSection.tsx
@@ -2,15 +2,9 @@
 /* eslint-disable jsx-a11y/alt-text */
 import { Flex, Button, Box, Text, Image, Heading } from "@chakra-ui/react";
 import React, { useState } from "react";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-// デフォルトのテーマ
-import "@splidejs/react-splide/css";
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
 
-// または、ほかのテーマ
-import "@splidejs/react-splide/css/skyblue";
-
-// あるいは、コアスタイルのみ
-import "@splidejs/react-splide/css/core";
+import "@splidejs/react-splide/css/skyBlue";
 
 export const CarCatalogSection = () => {
   const [carCatalogState, setCarCatalogState] = useState<CarCatalog[]>([
@@ -117,98 +111,100 @@ export const CarCatalogSection = () => {
         <Box w="100%">
           <Splide
             options={{
-              autoHeight: true,
-              autoWidth: true,
               pagination: false,
-              gap: "1rem",
+              gap: 20,
               focus: "center",
               trimSpace: false,
               autoplay: true,
               rewind: true,
-              
+              autoHeight: true,
+              autoWidth: true,
             }}
             aria-label="新型車"
-            style={{ padding: 0, marginTop: 40, }}
+            style={{ padding: 0, marginTop: 40 }}
+            hasTrack={false}
           >
-            {carCatalogState.map((car, index) => {
-              return (
-                <SplideSlide style={{ margin: "10px" }}>
-                  <Flex
-                    w="230px"
-                    boxShadow="md"
-                    p="3px"
-                    h="200px"
-                    flexDirection="column"
-                    justifyContent="space-around"
-                  >
-                    <Image
-                      src={`/img/catalog/slideshow/${car.image}.png`}
-                      h="110px"
-                    />
-                    <Flex justifyContent="space-between">
-                      <Flex flexDirection="column" w="110px">
-                        <Text fontSize="1px">{car.maker}</Text>
-                        <Text
-                          fontSize="1px"
-                          textOverflow="ellipsis"
-                          whiteSpace="nowrap"
-                          overflow="hidden"
+            <SplideTrack>
+              {carCatalogState.map((car, index) => {
+                return (
+                  <SplideSlide style={{ margin: "10px 0" }}>
+                    <Flex
+                      w="230px"
+                      boxShadow="md"
+                      p="3px"
+                      h="200px"
+                      flexDirection="column"
+                      justifyContent="space-around"
+                    >
+                      <Image
+                        src={`/img/catalog/slideshow/${car.image}.png`}
+                        h="110px"
+                      />
+                      <Flex justifyContent="space-between">
+                        <Flex flexDirection="column" w="110px">
+                          <Text fontSize="1px">{car.maker}</Text>
+                          <Text
+                            fontSize="1px"
+                            textOverflow="ellipsis"
+                            whiteSpace="nowrap"
+                            overflow="hidden"
+                          >
+                            {car.name}
+                          </Text>
+                        </Flex>
+
+                        <Button
+                          borderRadius=""
+                          p="5px"
+                          m="0"
+                          w=""
+                          h=""
+                          bg="white"
+                          boxShadow={car.bookmark ? "" : "md"}
+                          fontWeight="normal"
+                          onClick={() => handleBookmark(index)}
                         >
-                          {car.name}
-                        </Text>
+                          {car.bookmark ? (
+                            <Text fontSize="1px">
+                              お気に入り<br></br>解除
+                            </Text>
+                          ) : (
+                            <Text fontSize="1px">
+                              お気に入りに<br></br>追加
+                            </Text>
+                          )}
+                          <Image
+                            src={`/img/icon/${
+                              car.bookmark ? "pinCheck" : "pinUnCheck"
+                            }.png`}
+                            w="25px"
+                            ml="10px"
+                          ></Image>
+                        </Button>
                       </Flex>
 
-                      <Button
-                        borderRadius=""
-                        p="5px"
-                        m="0"
-                        w=""
-                        h=""
-                        bg="white"
-                        boxShadow={car.bookmark ? "" : "md"}
-                        fontWeight="normal"
-                        onClick={() => handleBookmark(index)}
-                      >
-                        {car.bookmark ? (
-                          <Text fontSize="1px">
-                            お気に入り<br></br>解除
-                          </Text>
-                        ) : (
-                          <Text fontSize="1px">
-                            お気に入りに<br></br>追加
-                          </Text>
-                        )}
-                        <Image
-                          src={`/img/icon/${
-                            car.bookmark ? "pinCheck" : "pinUnCheck"
-                          }.png`}
-                          w="25px"
-                          ml="10px"
-                        ></Image>
-                      </Button>
+                      <Box textAlign="center" mt="10px" mb="10px">
+                        <Button
+                          bg="white"
+                          border="1px solid #2D2D2D"
+                          borderRadius="2px"
+                          fontWeight=""
+                          w="130px"
+                          h="30px"
+                          justifyContent="space-around"
+                          backgroundImage="/img/icon/arrowRightSlideButton.png"
+                          backgroundRepeat="no-repeat"
+                          backgroundPosition="95% 50%"
+                          backgroundSize="11px"
+                        >
+                          <Text fontSize="13px">詳細を見る</Text>
+                        </Button>
+                      </Box>
                     </Flex>
-
-                    <Box textAlign="center" mt="10px" mb="10px">
-                      <Button
-                        bg="white"
-                        border="1px solid #2D2D2D"
-                        borderRadius="2px"
-                        fontWeight=""
-                        w="130px"
-                        h="30px"
-                        justifyContent="space-around"
-                        backgroundImage="/img/icon/arrowRightSlideButton.png"
-                        backgroundRepeat="no-repeat"
-                        backgroundPosition="95% 50%"
-                        backgroundSize="11px"
-                      >
-                        <Text fontSize="13px">詳細を見る</Text>
-                      </Button>
-                    </Box>
-                  </Flex>
-                </SplideSlide>
-              );
-            })}
+                  </SplideSlide>
+                );
+              })}
+            </SplideTrack>
           </Splide>
         </Box>
       </Flex>
